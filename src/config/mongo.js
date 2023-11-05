76b18f69323c2ac792473b4e17e1631e77dcb271
sirajsk <sirajkpd12@gmail.com>
2023-11-05 14:44:45 +0530

    mongo url changed

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,6 +1,3 @@
 module.exports = {
-    url: `mongodb://${process?.env?.MONGODB_HOST}:${process?.env?.MONGODB_PORT}`,
-    username: process?.env?.MONGODB_USERNAME,
-    password: process?.env?.MONGODB_PASSWORD,
-    dbName: process?.env?.MONGODB_DATABASE,
+    url: `mongodb+srv://influece:${process.env.MONGODB_PASSWORD}@influence.vwadurh.mongodb.net/?retryWrites=true&w=majority`,
 };
